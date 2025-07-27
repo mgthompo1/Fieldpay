c123c36feef9cfd0b46be49d948eb55e89e00eaa
Mitchell Thompson <mgthompo@gmail.com>
2025-07-26 23:58:09 -0500

    Initial Commit

---
+++ fieldpay/fieldpayApp.swift
@@ -0,0 +1,17 @@
+//
+//  fieldpayApp.swift
+//  fieldpay
+//
+//  Created by Mitchell Thompson on 7/26/25.
+//
+
+import SwiftUI
+
+@main
+struct fieldpayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fieldpayTests/fieldpayTests.swift
@@ -0,0 +1,17 @@
+//
+//  fieldpayTests.swift
+//  fieldpayTests
+//
+//  Created by Mitchell Thompson on 7/26/25.
+//
+
+import Testing
+@testable import fieldpay
+
+struct fieldpayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
